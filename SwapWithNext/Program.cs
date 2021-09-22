d60cc932eda1ab08949b7eaf557d31b579db27f2
hxchua00 <hxchua00@gmail.com>
2021-09-23 02:56:29 +0800

    Add project files.

---
+++ SwapWithNext/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapWithNext
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string: ");            
+            string s = Console.ReadLine();
+            Console.WriteLine();
+
+            SwapNext mySwap = new SwapNext(s);
+            mySwap.SwapWNext(s);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SwapWithNext/SwapNext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapWithNext
+{
+    class SwapNext
+    {
+        string newStr;
+
+        public SwapNext(string newStr)
+        {
+            this.newStr = newStr;
+        }
+
+        public void SwapWNext(string newStr)
+        {
+            char[] characters = new char[] { 'a','b','c','d','e','f','g',
+                                             'h','i','j','k','l','m','n',
+                                             'o','p','q','r','s','t','u',
+                                             'v','w','x','y','z','a'};
+            char[] strArr = new char[newStr.Length];
+
+            for(int i=0; i < newStr.Length; i++)
+            {
+                strArr[i] = newStr[i];
+            }
+
+            char[] newStrArr = new char[newStr.Length];
+            char temp = 'a';
+
+            for (int i = 0; i < strArr.Length; i++)
+            {
+                for (int j = 0; j < characters.Length-1; j++)
+                {
+                    if (strArr[i] == characters[j])
+                    {
+                        temp = characters[j + 1];
+                    }
+                }
+                newStrArr[i] = temp;
+            }
+
+            Console.WriteLine("New string: ");
+            for (int i = 0; i < newStrArr.Length; i++)
+            {
+                Console.Write(newStrArr[i]); 
+            }
+            Console.WriteLine();
+        }
+    }
+}
